b08d16c95d4bf12c18c5250de9ce47de6a0caa2f
HarmanHans <harmanhans56@gmail.com>
2023-09-29 19:23:50 -0700

    mostly functional nav bar

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const header = document.querySelector('header');
+const navToggle = document.querySelector('.nav-toggle');
+const navBar = document.querySelector('.navbar');
 
 let prevScrollPos = window.pageYOffset + 120;
-console.log(prevScrollPos);
 
 window.onscroll = function() {
     let currentScrollPos = window.pageYOffset;
@@ -9,3 +10,7 @@ window.onscroll = function() {
     prevScrollPos > currentScrollPos ? header.classList.remove('scroll') : header.classList.add('scroll');
     prevScrollPos = currentScrollPos;
 }
+
+navToggle.addEventListener('click', () =>  {
+    navBar.classList.toggle('hide');
+});
