12751d1fa17b33db632521e8acb1a7ab3bfc29ae
HarmanHans <harmanhans56@gmail.com>
2023-10-05 23:00:19 -0700

    added animations

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ const titles = document.querySelectorAll('h1.section__title');
 
 let prevScrollPos = window.pageYOffset + 70;
 
-console.log(titles);
-
 window.addEventListener('scroll', hideNav);
 
-for (let i = 0; i < titles.length; i++) {
+/*for (let i = 0; i < titles.length; i++) {
     window.addEventListener('scroll', shiftHeader(titles[i]));
-}
+}*/
 
 function hideNav() {
     let currentScrollPos = window.pageYOffset;
@@ -20,13 +18,13 @@ function hideNav() {
     prevScrollPos = currentScrollPos;
 }
 
-function shiftHeader(element) {
+/*function shiftHeader(element) {
     let currentScrollPos = window.pageYOffset;
 
     prevScrollPos > currentScrollPos ? element.classList.remove('animation') : element.classList.add('animation');
 
     prevScrollPos = currentScrollPos;
-}
+}*/
 
 window.addEventListener('resize', function() {
     window.innerWidth > 630 ? navBar.classList.remove('hide') : navBar.classList.add('hide');
@@ -35,3 +33,51 @@ window.addEventListener('resize', function() {
 navToggle.addEventListener('click', () =>  {
     navBar.classList.toggle('hide');
 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('drop-in');
+            entry.target.classList.add('show');
+        } /*else {
+            entry.target.classList.remove('drop-in');
+        } */
+    });
+});
+
+const observerBody = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animation');
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const observerSlide = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('slide-up');
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const linkSlide = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const dropElements = document.querySelectorAll('.nav__item');
+const titleElements = document.querySelectorAll('h1.section__title');
+const slideElements = document.querySelectorAll('.slides');
+const linkElements = document.querySelectorAll('.links__list');
+console.log(slideElements);
+dropElements.forEach((el) => observer.observe(el));
+
+titleElements.forEach((el) => observerBody.observe(el));
+slideElements.forEach((el) => observerSlide.observe(el));
+linkElements.forEach((el) => linkSlide.observe(el));
