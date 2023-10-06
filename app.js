cc4298f6d0c1499829621710835b19d15da3f922
HarmanHans <harmanhans56@gmail.com>
2023-10-05 20:37:24 -0700

    more design changes

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const header = document.querySelector('header');
 const navToggle = document.querySelector('.nav-toggle');
 const navBar = document.querySelector('.navbar');
+const titles = document.querySelectorAll('h1.section__title');
 
-let prevScrollPos = window.pageYOffset + 120;
+let prevScrollPos = window.pageYOffset + 70;
 
-window.onscroll = function() {
+console.log(titles);
+
+window.addEventListener('scroll', hideNav);
+
+for (let i = 0; i < titles.length; i++) {
+    window.addEventListener('scroll', shiftHeader(titles[i]));
+}
+
+function hideNav() {
     let currentScrollPos = window.pageYOffset;
 
     prevScrollPos > currentScrollPos ? header.classList.remove('scroll') : header.classList.add('scroll');
+    prevScrollPos = currentScrollPos;
+}
+
+function shiftHeader(element) {
+    let currentScrollPos = window.pageYOffset;
+
+    prevScrollPos > currentScrollPos ? element.classList.remove('animation') : element.classList.add('animation');
+
     prevScrollPos = currentScrollPos;
 }
 
